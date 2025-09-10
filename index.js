2b53f31475dc5f4022838b3aa54cfcbd682e2c85
krowbee <skoresplay@gmail.com>
2025-09-10 13:14:25 +0300

    Work done

---
+++ index.js
@@ -0,0 +1,56 @@
+let responseData = "";
+const defaultPeriod = "weekly"
+const buttons = document.querySelectorAll(".cards__time-button")
+
+
+const renderCards = (period) =>{
+
+    const prevPeriodMessages = {
+        "daily": "Last day - ",
+        "weekly": "Last week - ",
+        "monthly": "Last month - "
+    }
+
+    const cards = document.querySelectorAll(".cards__card[data-type='activity']")
+    cards.forEach((card, index) => {
+        const mainHours = card.querySelector(".cards__hours-main")
+        const prevHours = card.querySelector(".cards__hours-previous")
+        mainHours.textContent = responseData[index].timeframes[period].current + "hrs"
+        prevHours.textContent = prevPeriodMessages[period] + responseData[index].timeframes[period].previous + "hrs"
+    })
+}
+
+const timeSwitch = (button) =>{
+    activeButton = document.querySelector(".cards__time-button[active]")
+    const period = button.dataset.period
+
+    try{
+        activeButton.toggleAttribute("active")
+        button.toggleAttribute("active")
+        
+        renderCards(period)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+const fetchData =  async () =>{
+    try{
+        const response = await fetch("./data.json");
+        if (!response.ok){
+            throw new Error('Response status: ${response.status}')
+        }
+
+        responseData = await response.json()
+        renderCards(defaultPeriod)
+
+    } catch (error){
+        console.log(error.message)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", fetchData)
+
+buttons.forEach((button)=>{
+    button.addEventListener("click", () => timeSwitch(button))
+})
